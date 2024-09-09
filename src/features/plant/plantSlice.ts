c496e9ad23043f57f9cb0b306e9fea5ab3ba004b
Nika Kolesnikova <firstnamenika@gmail.com>
2024-09-09 14:07:13 -0400

    add more reducer methods

--- src/features/plant/plantSlice.ts
+++ src/features/plant/plantSlice.ts
@@ -4,12 +4,13 @@ import { createAppSlice } from "../../app/createAppSlice"
 
 export type Plant = {
     plantId: number | null,
-    parentOf: number[] | null,
+    parentOf: number | number[] | null,
     childOf: number | null,
     name: string,
+    image: string,
     dateCreated: Date | null,
     lastRepotted: Date | null,
-    soilMix: string,
+    soilMixRecipe: string,
     familyTreeId: number | null,
     timelineId: number | null,
 }
@@ -23,30 +24,64 @@ export const initialState: PlantArray = {
 }
 
 export const plantSlice = createAppSlice({
-    name:"plants",
+    name: "plants",
     initialState,
     reducers: {
-        addNewPlant: (state, action: PayloadAction<Plant>) => {
-            state.plants.push(action.payload);
-          },
-        deletePlant: (state, action: PayloadAction<number>) => {
-            state.plants = state.plants.filter((plant) => plant.plantId !== action.payload);
-          },
-        updatePlant: (state, action: PayloadAction<{ id: number; title: string }>) => {
-            state.plants = state.plants.map((plant) => {
-              if (plant.plantId === action.payload.id) {
-                return { ...plant, title: action.payload.title };
-              }
-              return plant;
-            });
-          }
-        },
-        selectors: {
-            selectPlantsExist: state => state.plants.length > 0,
+      addNewPlant: (state, action: PayloadAction<Plant>) => {
+        state.plants.push(action.payload);
       },
-});
+      deletePlant: (state, action: PayloadAction<number>) => {
+        state.plants = state.plants.filter((plant) => plant.plantId !== action.payload);
+      },
+      updatePlant: (state, action: PayloadAction<{ id: number; title: string }>) => {
+        state.plants = state.plants.map((plant) => {
+          if (plant.plantId === action.payload.id) {
+            return { ...plant, title: action.payload.title };
+          }
+          return plant;
+        });
+      },
+    //   this function will be responsible for creating a new timeline in the backend
+      updatePlantImage: (state, action: PayloadAction<{ plantId: number; newImage: string }>) => {
+        state.plants = state.plants.map((plant) => {
+          if (plant.plantId === action.payload.plantId) {
+            return { ...plant, image: action.payload.newImage };
+          }
+          return plant;
+        });
+      },
+      repotPlant: (state, action: PayloadAction<{plantId: number, newRepottingDate: Date}>) => {
+        state.plants = state.plants.map((plant) => {
+            if (plant.plantId === action.payload.plantId) {
+              return { ...plant, lastRepotted: action.payload.newRepottingDate };
+            }
+            return plant;
+          });
+      },
+      makeAPup: (state, action: PayloadAction<{parentId: number, childId: number}>) => {
+        state.plants = state.plants.map((plant) => {
+            if (plant.plantId === action.payload.parentId) {
+              return { ...plant, parentOf: action.payload.childId };
+            }
+            return plant;
+          });
+      },
+      soilMixRecipe: (state, action: PayloadAction<{plantId: number, newSoilMixRecipe: string}>) => {
+        state.plants = state.plants.map((plant) => {
+            if (plant.plantId === action.payload.plantId) {
+              return { ...plant, soilMixRecipe: action.payload.newSoilMixRecipe };
+            }
+            return plant;
+          });
+      },
+    },
+       selectors: {
+            selectPlantsExist: state => state.plants.length > 0,
+    }
+  });
 
-export const { addNewPlant, updatePlant, deletePlant } = plantSlice.actions;
+export const { addNewPlant, updatePlant, updatePlantImage, deletePlant, 
+               repotPlant, makeAPup, soilMixRecipe } = plantSlice.actions;
 
 export const { selectPlantsExist } = plantSlice.selectors
 
