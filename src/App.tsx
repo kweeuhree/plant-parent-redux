41aa05f3ba7e1f2ead4e7a4e965648489c076d3a
Nika Kolesnikova <firstnamenika@gmail.com>
2024-09-14 12:43:23 -0400

    feat: add routes and update imports in App component
    - Added routes for plant display, adding, and updating, as well as profile and login pages.
    - Moved all imports to follow the CSS import in the App component.
    
    This update improves the organization of routes and ensures that the CSS is loaded before other components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import "./App.css"
-
+import { Route, Routes } from 'react-router-dom';
+import SignUpLoginForm from "./features/user/SignUpLoginForm";
+import ProtectedRoute from "./features/user/ProtectedRoute";
 
 const App = () => {
+
   return (
     <div className="App">
-      
+      <Routes>
+          <Route path="/" element={<ProtectedRoute element={AllPlantDisplay} />} />
+          <Route path="/add-new-plant" element={<ProtectedRoute element={PlantForm} formType="ADD" />} />
+          <Route path="/update-plant" element={<ProtectedRoute element={PlantForm} formType="UPDATE" />} />
+          <Route path="/profile" element={<ProtectedRoute element={Profile} />} />
+          <Route path="/" element={<SignUpLoginForm formType="LOGIN" />} /> 
+      </Routes>
     </div>
   )
 }
