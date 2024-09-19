5d5c7278fc41cb23e14264bf4467b618d46c0f07
Nika Kolesnikova <firstnamenika@gmail.com>
2024-09-19 15:01:48 -0400

    feat: enhance SubmitButton by passing optional text and onClick props

--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -1,7 +1,14 @@
-const SubmitButton = () => {
+import type { MouseEventHandler } from "react";
+
+type Props = {
+  text?: string,
+  onClick?: MouseEventHandler<HTMLButtonElement>,
+}
+
+const SubmitButton = ({text = 'Submit', onClick}: Props) => {
   return (
-    <button type="submit">Submit</button>
-  )
+    <button type="submit" onClick={onClick}>{text}</button>
+  );
 }
 
 export default SubmitButton;
