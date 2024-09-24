699c6441c5e1d197fa55b7e9509f99e7fb8cd0ef
Nika Kolesnikova <firstnamenika@gmail.com>
2024-09-24 10:11:57 -0400

    feat: use useNavigateToPath in AllPlantsDisplat

--- src/features/plant/AllPlantsDisplay.tsx
+++ src/features/plant/AllPlantsDisplay.tsx
@@ -1,12 +1,16 @@
+import { useNavigateToPath } from "../../app/hooks"
 // plant slice imports
 import PlantPreview from "./PlantPreview"
 import type { Plant } from './plantSlice'
+// components
+import Button from "../../components/Button"
 
 type Props = {
   plants: Plant[],
 }
 
 const AllPlantsDisplay = ( { plants }: Props ) => {
+  const navigate = useNavigateToPath('');
 
   const plantsList = plants?.map((plant) => (
     <PlantPreview  
@@ -14,6 +18,7 @@ const AllPlantsDisplay = ( { plants }: Props ) => {
       plant={plant}
        />
   ))
+
 
   return (
     <>
@@ -26,6 +31,7 @@ const AllPlantsDisplay = ( { plants }: Props ) => {
         'no plants to display'
       )
     }
+    <Button onClick={() => navigate('/profile')} text="Profile" />
     </>
   )
 }
