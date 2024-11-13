3ab91c9c918e2c0a0d198623ec408f556fbf849a
Nika Kolesnikova <firstnamenika@gmail.com>
2024-11-13 18:19:17 -0500

    fix: fix a type in App.test

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,11 +1,7 @@
 import { render } from "@testing-library/react";
 
-import { AppPro}
 import App from "../App";
 
 test("App should render", () => {
-  render(
-    
-  );
-
+  render(<App />);
 });
