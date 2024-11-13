fb31bd947eadb3f843957c3ab20258e6568879fd
Nika Kolesnikova <firstnamenika@gmail.com>
2024-11-13 17:08:42 -0500

    fix: organize imports and add semicolons

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,17 @@
-import React from "react"
-import { createRoot } from "react-dom/client"
-import { Provider } from "react-redux"
-import App from "./App"
-import { store } from "./app/store"
-import "./index.css"
-import { BrowserRouter as Router } from 'react-router-dom'
+import React from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import { createRoot } from "react-dom/client";
 
-const container = document.getElementById("root")
+import { store } from "./app/store";
+import App from "./App";
+
+import "./index.css";
+
+const container = document.getElementById("root");
 
 if (container) {
-  const root = createRoot(container)
+  const root = createRoot(container);
 
   root.render(
     <React.StrictMode>
@@ -19,9 +21,9 @@ if (container) {
         </Router>
       </Provider>
     </React.StrictMode>,
-  )
+  );
 } else {
   throw new Error(
     "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file.",
-  )
+  );
 }
